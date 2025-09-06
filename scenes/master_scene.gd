7e9141abec4ba33986e125f8383346ddfbd56458
Brandon P. Olynyk <brandontab2004@gmail.com>
2025-09-06 02:27:13 -0400

    ?

--- scenes/master_scene.gd
+++ scenes/master_scene.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var net_manager:NetworkManager = $NetworkManager
 var current_level
-
+@onready var level_spawner:MultiplayerSpawner = $LevelSpawner
 func _ready():
 	if OS.has_feature("dedicated_server"):
 		print('Starting Dedicated Server...')
@@ -11,8 +11,9 @@ func _ready():
 func load_level(path:String):
 	var loaded_level = load(path)
 	if current_level:
-		current_level.free()
+		current_level.queue_free()
 	var level_inst = loaded_level.instantiate()
 	add_child(level_inst)
+	level_spawner.spawn(level_inst)
 	current_level = level_inst
 	return current_level.id
